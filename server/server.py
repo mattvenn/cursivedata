678ed78268862da3eccbe339afbe53e38a4f30cd
Matt Venn <matt@mattvenn.net>
2012-09-08 16:29:24 +0100

    useing simplehttpserver for http post request handling

--- server/server.py
+++ server/server.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python
-import socket
 import sys
-import signal
 import json
-
-portnum = int(sys.argv[1])
-
-def signal_handler(signum, frame):
-    print "closing socket"
-    global sock
-    sock.close()
-    exit(1)
-
-signal.signal(signal.SIGINT, signal_handler)
-
+import argparse
+import SocketServer
+import SimpleHTTPServer
+import urllib
+import os
 
 def extractData(line):
     jsondata = urllib.unquote(line)
@@ -24,6 +16,7 @@ def extractData(line):
     return (light,time)
 
 def processData((light,time)):
+    global args
     import iso8601
     date = iso8601.parse_date(time)
     minute = int( date.strftime("%M") ) # minute 0 -59
@@ -34,13 +27,11 @@ def processData((light,time)):
 
     import subprocess
     print >>sys.stderr, "building squares"
-    subprocess.call(["./squares.py", "--rotate", "20", "--number", str(mins), "--env", str(int(float(light)))])
-    import os
-    #replace this with doing the subprocess stuff properly and capturing exit code and output
-    if os.path.isfile("square.svg"):
-      print >>sys.stderr, "new robot data"
+    result = subprocess.call([args.svggen, "--rotate", "20", "--number", str(mins), "--env", str(int(float(light)))])
+    if result == 0:
+      print >>sys.stderr, "new svg"
       #run the pycam stuff here
-      result = subprocess.call(["../../pycam-0.5.1/pycam", "square.svg", "--export-gcode=square.ngc", "--process-path-strategy=engrave"])
+      result = subprocess.call([args.pycam, "square.svg", "--export-gcode=square.ngc", "--process-path-strategy=engrave"])
       if result == 0: #unix for all good
         print >>sys.stderr, "process gcode to polar code"
         p = subprocess.Popen(["./preprocess.py", "--file", "square.ngc"], stdout=subprocess.PIPE)
@@ -48,70 +39,57 @@ def processData((light,time)):
         fh = open( "square.polar", "w" )
         fh.write(gcode)
         fh.close()
+        print "written polar code"
 
         #move the svg file to history with a timestamp
         newfile = "./history/" + str(seconds) + ".svg" 
         os.rename("square.svg", newfile)
 
-        """
-        #send the data to robot
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-          client_socket.connect(("localhost", 5000))
-          client_socket.send(gcode)
-          client_socket.close()
-          print >>sys.stderr, "sent data to robot"
-        except:
-          print "connection to robot failed"
-        """
       else:
         #move the svg file to history with a timestamp
         newfile = "./bustsvg/" + str(seconds) + ".svg" 
         os.rename("square.svg", newfile)
+    else:
+        print "no svg"
         
-         
+class postHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        print >>sys.stderr, '>>>>connection from', self.client_address
+        content_len = int(self.headers.getheader('content-length'))
+        post_body = self.rfile.read(content_len)
+        processData( extractData(post_body) )
+        return
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def run_server(args):
+    # Bind the socket to the port
+    server_address = (args.hostname, args.port)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    server = SocketServer.ThreadingTCPServer(server_address, postHandler,False)
+    server.allow_reuse_address=True
+    server.server_bind()
+    server.server_activate()
+    server.serve_forever()
 
-# Bind the socket to the port
-server_address = ('mattvenn.net', portnum)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
-# Listen for incoming connections
-sock.listen(1)
+if __name__ == '__main__':
+  argparser = argparse.ArgumentParser(
+      description="listens for updates from cosm and turns them into gcodes by running an external program")
+  argparser.add_argument('--hostname',
+      action='store', dest='hostname', default="localhost",
+      help="hostname")
+  argparser.add_argument('--port',
+      action='store', dest='port', type=int, default=10001,
+      help="port")
+  argparser.add_argument('--startenv',
+      action='store', dest='startenv', type=int, default=0,
+      help="where to start from")
+  argparser.add_argument('--pycam',
+      action='store', dest='pycam', default="/usr/bin/pycam",
+      help="where pycam is")
+  argparser.add_argument('--svggen',
+      action='store', dest='svggen', default="./squares.py",
+      help="where the svg generator program is")
 
-while True:
-    # Wait for a connection
-    print >>sys.stderr, '-----------------'
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
+  args = argparser.parse_args()
+  run_server(args)
 
-    try:
-        print >>sys.stderr, 'connection from', client_address
 
-        # Receive the data in small chunks and retransmit it
-        import urllib
-        while True:
-            try:
-                data = connection.recv(4096)
-                if data:
-                    for line in data.split("\n"):
-                        if line.startswith("body="):
-                            processData( extractData(line) )
-			    break
-
-                else:
-                    print >>sys.stderr, 'no more data from', client_address
-                    break
-            except:
-                e = sys.exc_info()[0]
-                print e
-                connection.close()
-                sock.close()
-                exit(1)
-            
-    finally:
-        # Clean up the connection
-        connection.close()
