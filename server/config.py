2de4d6d25ba1bffcf0d5dc3b23020d5f0dac2309
Matt Venn <matt@mattvenn.net>
2012-10-05 10:46:38 +0100

    starting point for config

---
+++ server/config.py
@@ -0,0 +1,19 @@
+
+config = {
+    46756 :
+    {
+        "name" : "Bristol Hackspace Light Level",
+        "draw_args" : ["--rotate", "20" ],
+        "generator" : "../generators/squares/squares.py",
+        "needs_polar" : True,
+    },
+    11111 :
+    {
+        "name" : "Solar Tree",
+        "draw_args" : [],
+        "generator" : "../generators/tree/tree.py",
+        "needs_polar" : False,
+    },
+}
+        
+       
