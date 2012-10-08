2b29d37c590f90b72a89473128334fa7815f1d29
Matt Venn <matt@mattvenn.net>
2012-10-08 23:17:48 +0400

    got rid of mistakenly added tree.svg, added new tree config to config

--- server/config.py
+++ server/config.py
@@ -10,7 +10,7 @@ config = {
     75479 :
     {
         "name" : "Solar Tree",
-        "draw_args" : [ "--scale", "100", "--debug" ],
+        "draw_args" : [ "--scale", "100", "--debug", '--height', '500', '--width', '500'],
         "start_daily" : True,
         "generator" : "../generators/tree/tree.py",
         "needs_polar" : False,
