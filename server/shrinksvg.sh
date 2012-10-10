78922cfe80253e5c524bfe2a527d50a0ead11e9b
Matt Venn <matt@mattvenn.net>
2012-10-10 20:15:46 +0100

    slightly more sophisticated to deal with multi dir and multi filename

--- server/shrinksvg.sh
+++ server/shrinksvg.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-cd /home/matt/polargraphenergymonitor/server
-mv concat.svg concat.svg.big
-grep -v '^$' concat.svg.big > concat.svg
-rm concat.svg.big
+feeds=$(\ls ../tmp)
+for feed in $feeds ; do
+  echo $feed
+  cd /home/matt/polargraphenergymonitor/tmp/$feed
+  if [ -f concat.svg ]; then
+    file="concat.svg"
+  elif [ -f tree.svg ]; then
+    file="tree.svg"
+  fi
+  echo $file
+  mv $file $file.big
+  grep -v '^$' $file.big > $file
+  rm $file.big
+done
+exit
+
