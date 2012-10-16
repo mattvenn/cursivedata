6612420202a24abdcc7530ee2c68b3c0839699dd
Matt Venn <matt@mattvenn.net>
2012-10-16 14:34:48 +0400

    added tmp dir

--- history/feedgen.py
+++ history/feedgen.py
@@ -4,13 +4,16 @@ import subprocess
 fh=open("history.json")
 d=json.load(fh)
 generator_dir = "../generators/tree/"
+tmp_dir = "../tmp/75479/"
 generator = "tree.py"
 line_num = 0
 for datum in d["power-in"]:
   print datum['at'],datum['value']
   val = int(float(datum['value']))
-
-  line_num += 1
-  gen_args = [generator_dir + generator, "--env", str(val), "--number", str(line_num), "--height", "500", "--width", "500", "--dir", generator_dir]
+  
+  gen_args = [generator_dir + generator, "--env", str(val), "--number", str(line_num), "--height", "400", "--width", "400", "--dir", tmp_dir ]
+  if line_num == 0:
+    gen_args.append( "--wipe" )
   print gen_args
   result = subprocess.call(gen_args)
+  line_num += 1
