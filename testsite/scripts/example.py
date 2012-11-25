3217709cb1259c368b5fb77ec13527cd43311716
mo-seph <dave@mo-seph.com>
2012-11-25 14:19:05 +0000

    Expanding functionality of example script

--- testsite/scripts/example.py
+++ testsite/scripts/example.py
@@ -3,3 +3,8 @@ def process(data,state) :
 	print "Example Processing data with",map(str,state.generatorstateparameter_set.all())
 	return "Example Changing Processing data with"+str.join("",map(str,state.generatorstateparameter_set.all()))
 
+def get_params() :
+	return  [ { "name":"Width" }, { "name":"Height" } ]
+
+def get_name() : return "Example Generator"
+def get_description() : return "Description of Example Generator"
