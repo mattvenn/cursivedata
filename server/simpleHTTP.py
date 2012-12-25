54c785af0abd38dc9723b004ec993b08de27453e
Matt Venn <matt@mattvenn.net>
2012-12-25 19:21:17 +0000

    spruced up robot data server with proper arguments, better error handling

--- server/simpleHTTP.py
+++ server/simpleHTTP.py
@@ -5,11 +5,10 @@ import urllib
 import os
 import datetime
 import sys
+import argparse
 from TimeStats import TimeStats
 
 nanodeGetTimes = TimeStats()
-dir = "/home/matt/polargraphenergymonitor/tmp/46756/"
-PORT = 10002
 
 class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
   def do_GET(self):
@@ -18,34 +17,63 @@ class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
     time_span = 10 * 60
     print >>sys.stderr, '>>>>connection from %s [%d in %dsecs]' % ( self.client_address, nanodeGetTimes.howMany(time_span),time_span)
     try:
-      fh = open(dir+"square.polar")
-      print "sending file: square.polar"
+      fh = open(args.dir+args.file)
+      print "sending file: ", args.file
       gcode = fh.read()
       fh.close()
       self.send_response(200)
       self.send_header('Content-type','text/html')
       self.end_headers()
       self.wfile.write(gcode)
-      #then remove it
-      print "moving old file"
-      try:
-        now = datetime.datetime.now()
-        seconds = now.strftime("%s")
-        newfile = "./oldpolar/" + str(seconds) + ".polar" 
-        os.rename(dir+"square.polar", newfile)
-      except:
-        e = sys.exc_info()[0]
-        print "couldn't move file", e
-    except:
-      print "no new data"
+      #then remove it if necessary
+      if not args.noremove:
+          try:
+            now = datetime.datetime.now()
+            seconds = now.strftime("%s")
+            newfile = "./oldpolar/" + str(seconds) + ".polar" 
+            print "moving old file to: ", newfile
+            os.rename(args.dir+args.file, newfile)
+          except OSError, e:
+            print "couldn't move file", e
+    except IOError, e:
+      print "no new data", e
       self.send_response(404)
       self.send_header('Content-type','text/html')
       self.end_headers()
     return
 
-print "serving at port", PORT
-server = SocketServer.ThreadingTCPServer(("mattvenn.net", PORT), Proxy,False)
-server.allow_reuse_address=True
-server.server_bind()
-server.server_activate()
-server.serve_forever()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="serves the polargraph files to the robot and then archives them",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--dir',
+        action='store', dest='dir', default="/home/matt/polargraphenergymonitor/tmp/46756/",
+        help="where the file is to be served, and the archive directory")
+    parser.add_argument('--file',
+        action='store', dest='file', default="square.polar",
+        help="the file to send")
+    parser.add_argument('--hostname',
+        action='store', dest='hostname', default="mattvenn.net",
+        help="hostname to listen on")
+    parser.add_argument('--port',
+        action='store', dest='port', type=int, default=10002,
+        help="tcp port to listen on")
+    parser.add_argument('--no-remove',
+        action='store_const', const=True, dest='noremove', default=False,
+        help="don't remove the file after serving, useful for testing")
+    parser.add_argument('--verbose',
+        action='store_const', const=True, dest='verbose', default=False,
+        help="verbose")
+
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.dir):
+        print "no such directory: ", args.dir
+        exit(1)
+
+    print "serving at %s:%d" % (args.hostname, args.port)
+    server = SocketServer.ThreadingTCPServer((args.hostname, args.port), Proxy,False)
+    server.allow_reuse_address=True
+    server.server_bind()
+    server.server_activate()
+    server.serve_forever()
