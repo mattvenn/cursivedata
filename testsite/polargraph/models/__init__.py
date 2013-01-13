f1c1e144cca22cbe78b5398749ce899f526c06c9
mo-seph <dave@mo-seph.com>
2013-01-13 16:09:52 +0000

    Moved main to pipeline for clarity

--- testsite/polargraph/models/__init__.py
+++ testsite/polargraph/models/__init__.py
@@ -3,5 +3,5 @@ from polargraph.models.cosm import *
 from polargraph.models.data import *
 from polargraph.models.endpoint import *
 from polargraph.models.generator import *
-from polargraph.models.main import *
+from polargraph.models.pipeline import *
 __all__ = ['COSMSource','DataStore','Endpoint','Generator','Parameter','GeneratorState','GeneratorStateParameter','Pipeline']
