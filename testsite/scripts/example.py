b2d72eb348f8b7cd168c3b73871e3caef7b850bd
mo-seph <dave@mo-seph.com>
2013-01-13 18:27:54 +0000

    Updating example to use parameters and do pretty things

--- testsite/scripts/example.py
+++ testsite/scripts/example.py
@@ -1,25 +1,37 @@
 import pysvg.text
+import colorsys
 
 def process(svg_document,data,params,internal_state) :
-    #print "Example Processing data with parameters",map(str,params)
+    print "Example Processing data with parameters",map(str,params)
     #print "Example internal state: ",internal_state
     #print "Internal State: ",internal_state.get("i","None")
     it = internal_state.get("i",0) 
     
     for point in data.get_current():
-        xp = ((it)%10+0)*10
-        yp = ((it)/10+0)*10 
-        w = ((float(point['value'])/34000.0))*10/2
+        num = params.get("Number",6)
+        cell_width = (100.0/num)
+        cws = str(cell_width)+"%"
+        xp = ((it)%num)*cell_width
+        yp = (((it)/num)%num)*cell_width
+        w = ((float(point['value'])/34000.0))*cell_width/2
         wp = str(w)+"%"
+        hue = float(point['value'])/34000.0
+        sat = params.get("Saturation",1.0)
+        lev = params.get("Level",0.5)
+        rgb = colorsys.hsv_to_rgb(hue, sat, lev)
+        rgbs = 'rgb('+str(int(rgb[0]*255.0))+','+str(int(rgb[1]*255.0))+','+str(int(rgb[2]*255.0))+')'
+        print "RGB:",rgb
+        print "RGBs:",rgbs
         build = pysvg.builders.ShapeBuilder()
-        svg_document.addElement(build.createRect(str(xp)+"%", str(yp)+"%", width="10%", height="10%", fill = "rgb(200, 200, 200)"))
-        svg_document.addElement(build.createCircle(str(xp+5)+"%", str(yp+5)+"%", r=wp, fill = "rgb(255, 0, 0)"))
+        svg_document.addElement(build.createRect(
+            str(xp)+"%", str(yp)+"%", width=cws, height=cws, fill="none", stroke="grey", strokewidth=1) )
+        svg_document.addElement(build.createCircle(str(xp+cell_width/2)+"%", str(yp+cell_width/2)+"%", r=wp, fill=rgbs))
         it = it+1
     internal_state["i"]=it
     return None
 
 def get_params() :
-    return  [ { "name":"Width" }, { "name":"Height" } ]
+    return  [ { "name":"Number" }, { "name":"Saturation" }, {"name":"Level"} ]
 
 def get_name() : return "Example Generator"
 def get_description() : return "Description of Example Generator"
