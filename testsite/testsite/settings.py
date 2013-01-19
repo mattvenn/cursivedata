ae6a886353d554dee6f6c53e058711169b436f8d
polarsite <polarsite@vennzaa1.miniserver.com>
2013-01-19 19:49:14 +0000

    andys mail settings

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -149,7 +149,7 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false'],
+            #'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
