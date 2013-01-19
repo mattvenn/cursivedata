1cf545e27d0ef33935718650fc19368234b42a42
Matt Venn <matt@mattvenn.net>
2013-01-19 17:58:32 +0000

    removed old methods, added a get_gcode_filename method

--- testsite/polargraph/models/endpoint.py
+++ testsite/polargraph/models/endpoint.py
@@ -11,6 +11,7 @@ from imp import find_module, load_module
 import json
 import csv
 import polargraph.svg as svg
+from polargraph.models.pipeline import StoredOutput
 import requests
 
 
@@ -19,19 +20,22 @@ import requests
 class Endpoint( models.Model ):
     name = models.CharField(max_length=200)
     device = models.CharField(max_length=200)
-    width = models.CharField(max_length=200)
-    height = models.CharField(max_length=200)
+    width = models.FloatField(max_length=200)
+    side_margin = models.FloatField(max_length=200)
+    top_margin = models.FloatField(max_length=200)
+    height = models.FloatField(max_length=200)
     url = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     
-    def add_svg(self,svg_file ):
-        gcode_file = svg.get_temp_filename("gcode")
-        svg.convert_svg_to_gcode(svg_file, gcode_file)
-        self.send_to_device(gcode_file)
-        
-    def send_to_device(self,gcode):
-        print "Sending gcode file",gcode,"to",self.device,"at",self.location
-        
+
+    #dave fix
+    def get_gcode_filename(self):
+        return "/tmp/test.polar"
+
+    def add_svg(self,svg_file,generator_params):
+        filename = self.get_gcode_filename()
+        svg.convert_svg_to_gcode(self,generator_params,svg_file,filename)
+    
     def __unicode__(self):
         return self.name
 
