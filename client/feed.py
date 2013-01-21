a560fbbb0204a4836f4548f9c3554ae3b1b6446b
matt venn <matt@mattvenn.net>
2013-01-21 17:44:38 +0000

    restored with most recent version from raspberry pi

--- client/feed.py
+++ client/feed.py
@@ -1,171 +1,114 @@
 #!/usr/bin/python
+"""
+main problem with reading and writing to the robot is that if we read too much we hang.
+if we don't read enough, the leonardo resets!
+"""
+import sys
+import json
 import re
 import sys
-import tty
 import time
+import datetime
 import string
 import argparse
 import signal
-import socket
+import serial
+import requests
 
-TIMEOUT = 0.5 # number of seconds your want for timeout
 
-def handler():
-  print "timed out on serial read"
+def send_status(response):
+    m=re.search('^top margin: ([0-9.]+)mm',response,re.M)
+    top_margin=m.group(1)
+    m=re.search('^side margin: ([0-9.]+)mm',response,re.M)
+    side_margin=m.group(1)
+    m=re.search('^h: ([0-9.]+)mm',response,re.M)
+    height=m.group(1)
+    m=re.search('^w: ([0-9.]+)mm',response,re.M)
+    width=m.group(1)
+    payload = {
+        'width': width,
+        "height": height,
+        "side_margin": side_margin,
+        "top_margin": top_margin,
+        }
+    #fix this
+    url = 'http://mattvenn.net:8080/api/v1/endpoint/1/' 
+    headers = {'content-type': 'application/json'}
+    r = requests.patch(url, data=json.dumps(payload),headers=headers)
+    print r.status_code
+    if r.status_code == 202:
+        print "updated ok"
+    else:
+        print "failed to update"
 
-def signal_handler(signum, frame):
-    print "closing socket"
-    global sock
-    sock.close()
-    exit(1)
+def fetch_data():
+    if args.verbose:
+        print "fetching from ", args.server
+    try:
+	r = requests.get(args.server)
+        if r.status_code == 200:
+	    gcodes = r.text
+	    if args.verbose:
+                print "got answer from server:"
+                print gcodes
+            return gcodes.splitlines()
+        else:
+            print "failed with ", r.status_code
 
-signal.signal(signal.SIGALRM, handler)
-signal.signal(signal.SIGINT, signal_handler)
+    except requests.exceptions.ConnectionError, e:
+        print >>sys.stderr, e.code
+        print >>sys.stderr, e.read()
+        return None
+
+
+def finish_serial():
+#  import pdb; pdb.set_trace()
+  if serial_port:
+      print "closing serial"
+      print serial_port
+      serial_port.close()
 
 """
 this requires the robot to respond in the expected way, where all responsed end with "ok"
 """
-def readResponse(args,serial,timeout=10):
+def read_serial_response():
+
   response = ""
-  #print "setting timeout to", timeout
+  all_lines = ""
   while string.find(response,"ok"):
-    try:
-      if(timeout > 0):
-        signal.alarm(timeout)
-      response = serial.readline()
-      signal.alarm(0)
-      if args.verbose:
-        print "<", response,
-    except:
-      print "timeout %d secs on read" % timeout
-      return
+    response = serial_port.readline()
+    if response == "":
+      print >>sys.stderr, "timeout on serial read"
+      finish_serial()
+      exit(1)
+    if args.verbose:
+      print "<- %s" % response,
+    all_lines += response
+  return all_lines
 
-def readFile(args):
-
+def readFile():
   try:
-    gcode = open( args.file)
+    gcode = open(args.file)
   except:
     print "bad file"
     exit(1)
   gcodes = gcode.readlines()
   return gcodes
 
-def initRobot(args):
-  serial = None
-  port = args.serialport
+def setup_serial():
   try:
-    serial = open( port, 'r+' )
-  except IOError:
-    print "robot not connected?"
+    serial_port=serial.Serial()
+    serial_port.port=args.serialport
+    serial_port.timeout=args.timeout
+    serial_port.baudrate=args.baud
+    serial_port.open()
+  except IOError, e:
+    print "robot not connected?", e
     exit(1)
-  tty.setraw(serial);
+  return serial_port
 
-  if args.home:
-    serial.write("c")
-    readResponse(args,serial,0)
-
-  print "speed and pwm"
-  #speed and pwm
-  serial.write("p%d,%d" % (args.speed, args.pwm ))
-  readResponse(args,serial)
-  #ms
-  print "microstep: %d, %d" % (MS0, MS1 )
-  serial.write("i%d,%d" % (MS0, MS1 ))
-  readResponse(args,serial)
-  #where are we
-  print "where are we?"
-  serial.write("q")
-  readResponse(args,serial)
-
-  return serial
-
-def writeToRobot(args,serial,gcodes):
-  p = re.compile( "^#" )
-  for line in gcodes:
-    if p.match(line):
-      print "skipping line:", line
-    elif not line == None:
-      print "> %s" % line
-      if not args.norobot:
-        serial.write(line)
-        readResponse(args,serial)
-
-def setupPort(args):
-  # Create a TCP/IP socket
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-  # Bind the socket to the port
-  server_address = ('localhost', args.port)
-  print >>sys.stderr, 'starting up on %s port %s' % server_address
-  sock.bind(server_address)
-  # Listen for incoming connections
-  sock.listen(1)
-  return sock
-
-def listenPort(args,serial,sock):
-  while True:
-      # Wait for a connection
-      print >>sys.stderr, '-----------------'
-      print >>sys.stderr, 'waiting for a connection'
-      connection, client_address = sock.accept()
-
-      try:
-          print >>sys.stderr, 'connection from', client_address
-          while True:
-              try:
-                  data = connection.recv(4096)
-                  if data:
-                    gcodes = data.split("\n")
-                    writeToRobot(args,serial,gcodes)
-                  else:
-                    print >>sys.stderr, 'no more data from', client_address
-                    break
-              except:
-                  e = sys.exc_info()[0]
-                  print e
-                  connection.close()
-                  sock.close()
-                  exit(1)
-      finally:
-        # Clean up the connection
-        connection.close()
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="feed polar files to polargraph robot")
-    parser.add_argument('--port',
-        action='store', dest='port', type=int,
-        help="port to listen on")
-    parser.add_argument('--serialport',
-        action='store', dest='serialport', default='/dev/ttyACM0',
-        help="serial port to listen on")
-    parser.add_argument('--file',
-        action='store', dest='file', 
-        help="file to open")
-    parser.add_argument('--verbose',
-        action='store_const', const=True, dest='verbose', default=False,
-        help="verbose")
-    parser.add_argument('--norobot',
-        action='store_const', const=True, dest='norobot', default=False,
-        help="no robot connected, for testing")
-    parser.add_argument('--home',
-        action='store_const', const=True, dest='home', default=False,
-        help="home to start")
-    parser.add_argument('--pwm',
-        action='store', dest='pwm', type=int, default=80,
-        help="pwm to draw")
-    parser.add_argument('--speed',
-        action='store', dest='speed', type=int, default=4,
-        help="speed to draw")
-    parser.add_argument('--ms',
-        action='store', dest='ms', type=int, default=0,
-        help="micro step: 0,1,2,3")
-
-
-    args = parser.parse_args()
-    #none
+def setup_robot():
+    #microstepping arguments
     if args.ms == 0:
       MS0 = 0
       MS1 = 0
@@ -182,20 +125,111 @@ if __name__ == '__main__':
       MS0 = 1
       MS1 = 1
 
+    setup_commands= [
+            #speed and pwm
+            "p%d,%d" % (args.speed, args.pwm ),
+            "i%d,%d" % (MS0, MS1 ),
+        ]
+    #home
+    if args.home:
+        setup_commands.append("c")
+    
+    send_robot_commands(setup_commands)
+
+
+def send_robot_commands(gcodes):
+  p = re.compile( "^#" )
+  response = ""
+  for line in gcodes:
+    if p.match(line):
+      print "skipping line:", line
+    elif not line == None:
+      print "-> %s" % line,
+      serial_port.write(line)
+      response += read_serial_response()
+  return response
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="feed polar files to polargraph robot")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--command', action='store', dest='command', help="command to send")
+    group.add_argument('--file', action='store', dest='file', help="file to open")
+    group.add_argument('--server', action='store', dest='server', help="server to check")
+
+    parser.add_argument('--baud',
+        action='store', dest='baud', type=int, default='57600',
+        help="baud rate")
+    parser.add_argument('--serialport',
+        action='store', dest='serialport', default='/dev/ttyACM0',
+        help="serial port to listen on")
+    parser.add_argument('--store',
+        action='store', dest='store_file', 
+        help="file to write robot responses in")
+    parser.add_argument('--verbose',
+        action='store_const', const=True, dest='verbose', default=False,
+        help="verbose")
+    parser.add_argument('--send-status',
+        action='store_const', const=True, dest='sendstatus', default=False,
+        help="send current status of the robot to the server")
+    parser.add_argument('--no-robot',
+        action='store_const', const=True, dest='norobot', default=False,
+        help="no robot connected, for testing")
+    parser.add_argument('--home',
+        action='store_const', const=True, dest='home', default=False,
+        help="home to start")
+    parser.add_argument('--setup-robot',
+        action='store_const', const=True, dest='setup_robot', default=False,
+        help="send pwm, ms etc commands to robot before the given file")
+    parser.add_argument('--pwm',
+        action='store', dest='pwm', type=int, default=80,
+        help="pwm to draw")
+    parser.add_argument('--speed',
+        action='store', dest='speed', type=int, default=4,
+        help="speed to draw")
+    parser.add_argument('--serial-timeout',
+        action='store', dest='timeout', type=int, default=1,
+        help="timeout on serial read")
+    parser.add_argument('--ms',
+        action='store', dest='ms', type=int, default=0,
+        help="micro step: 0,1,2,3")
+
+    args = parser.parse_args()
+
+
+    #send a file   
     if args.file:
-      gcodes = readFile(args)
-      if not args.norobot:
-        serial = initRobot(args)
-        writeToRobot(args,serial,gcodes)
-      print "finished"
-    elif args.port:
-      sock = setupPort(args)
-      if not args.norobot:
-        serial = initRobot(args)
-      else:
-        serial = None
-      listenPort(args,serial,sock)
-    else:
-      print "must specify either port or file"
-      exit(1)
+        gcodes = readFile()
+    #send a command
+    if args.command:
+        gcodes=[args.command+"\n"]
+    #use a remote server 
+    if args.server:
+        gcodes = fetch_data()
+
+    if args.sendstatus and args.server and not args.norobot:
+        serial_port = setup_serial()
+        status_commands=["u\n"]
+        response = send_robot_commands(status_commands)
+        finish_serial()
+        send_status(response)
+
+    if not gcodes:
+        print >>sys.stderr, "no gcodes found"
+        exit(1)
+
+    if not args.norobot:
+        time.sleep(1)
+        serial_port = setup_serial()
+
+        if args.setup_robot:
+          setup_robot()
+
+        response = send_robot_commands(gcodes)
+
+        if args.store_file:
+          store=open(args.store_file,'w+')
+          store.write(response)
+
+        finish_serial()
 
