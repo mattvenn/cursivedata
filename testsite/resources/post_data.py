b29fab06d4e519588da2add5de9f6fb841b0b07c
Matt Venn <matt@mattvenn.net>
2013-01-24 23:59:46 +0000

    improved description for args and added a minute parameter to set the datetime

--- testsite/resources/post_data.py
+++ testsite/resources/post_data.py
@@ -1,12 +1,13 @@
+#!/usr/bin/python
 import requests
 import json
+import datetime
 import pprint
 import argparse
 
 
 def push_data():
     headers= {'content-type': 'application/json'}
-      #"timestamp": "$TIME",
       #how much of this can be got rid of?
     data = {
       "environment": 
@@ -24,11 +25,12 @@ def push_data():
       },
       "id": 1,
       "threshold_value": 9.0,
+      "timestamp": timestamp,
       "triggering_datastream": 
       {
         "id": "0",
         "url": "http:\/\/api.cosm.com\/v2\/feeds\/343\/datastreams\/0",
-        "at": "2009-09-07T12:16:02.000063Z",
+        "at": timestamp,
         "value": 
         {
           "value": args.value,
@@ -40,12 +42,20 @@ def push_data():
       "url": "http:\/\/api.cosm.com\/v2\/triggers\/1"
     }
 
-    r = requests.post(args.url + str(args.pipeline) + "/",headers=headers,data=json.dumps(data))
+    r = requests.post(args.url + str(args.cosmsourceid) + "/",headers=headers,data=json.dumps(data))
     print r.status_code
     try:
         pprint.pprint(json.loads(r.text))
     except:
         print r.text
+
+def calculate_datetime_from_minute():
+   now = datetime.datetime.now()
+   hours = int(args.minute / 60)
+   mins = args.minute % 60
+   then = datetime.datetime(now.year,now.month,now.day,hours,mins,0)
+   return then.strftime("%Y-%m-%dT%H:%M:%SZ")
+
 
 if __name__ == '__main__':
 
@@ -53,16 +63,22 @@ if __name__ == '__main__':
 #    'http://mattvenn.net:8080/api/v1/cosm'
 
     parser = argparse.ArgumentParser(description="feed polar files to polargraph robot")
-    parser.add_argument('--pipeline',
-        action='store', dest='pipeline', type=int, default='1',
-        help="pipeline")
+    parser.add_argument('--cosmsourceid',
+        action='store', dest='cosmsourceid', type=int, default='1',
+        help="the id of the cosm source (not the pipeline)")
     parser.add_argument('--value',
         action='store', dest='value', type=float, default='1',
         help="value")
     parser.add_argument('--url',
         action='store', dest='url', default=default_url,
         help="url of the api")
+    parser.add_argument('--minute',
+        action='store', dest='minute', type=int, default='0',
+        help="specify a minute of the day to set date to")
 
     args = parser.parse_args()
     print args.url
+    if args.minute:
+        timestamp = calculate_datetime_from_minute()
+        print timestamp
     push_data()
