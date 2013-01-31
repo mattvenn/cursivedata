9e10f7f3db9091efd28001bd296aceb66f008150
Matt Venn <matt@mattvenn.net>
2013-01-31 09:13:04 +0000

    added status for endpoint

--- testsite/polargraph/models/endpoint.py
+++ testsite/polargraph/models/endpoint.py
@@ -20,7 +20,7 @@ class Endpoint( models.Model ):
     top_margin = models.FloatField(max_length=200)
     height = models.FloatField(max_length=200)
     #add this to db, using url for now
-    #status = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     
