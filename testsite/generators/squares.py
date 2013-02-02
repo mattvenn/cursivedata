9b230da9308844eba06dd20aa9ae261e2563946f
mo-seph <dave@mo-seph.com>
2013-02-02 18:09:36 +0000

    Fixed indentation which was a mix of 4 and 2 spaces

--- testsite/generators/squares.py
+++ testsite/generators/squares.py
@@ -11,20 +11,20 @@ import datetime
 import math
 
 def square(x,y,width,dwg,id,rotate):
-  points = []
-  hWidth = width/2
-  style_dict = { "fill":"none", "stroke":"#000", "stroke-width":"1" }
+    points = []
+    hWidth = width/2
+    style_dict = { "fill":"none", "stroke":"#000", "stroke-width":"1" }
 
-  p = path("M%d,%d" % (x-hWidth,y-hWidth))
-  p.appendLineToPath(x+hWidth,y-hWidth,False)
-  p.appendLineToPath(x+hWidth,y+hWidth,False)
-  p.appendLineToPath(x-hWidth,y+hWidth,False)
-  p.appendLineToPath(x-hWidth,y-hWidth,False)
-  p.set_style(StyleBuilder(style_dict).getStyle())
-  p.set_id(id)
-  if rotate:
-    p.set_transform("rotate(%d,%d,%d)" % (rotate,x,y))
-  dwg.addElement(p)
+    p = path("M%d,%d" % (x-hWidth,y-hWidth))
+    p.appendLineToPath(x+hWidth,y-hWidth,False)
+    p.appendLineToPath(x+hWidth,y+hWidth,False)
+    p.appendLineToPath(x-hWidth,y+hWidth,False)
+    p.appendLineToPath(x-hWidth,y-hWidth,False)
+    p.set_style(StyleBuilder(style_dict).getStyle())
+    p.set_id(id)
+    if rotate:
+        p.set_transform("rotate(%d,%d,%d)" % (rotate,x,y))
+    dwg.addElement(p)
 
 def get_minute(date):
     minute = int( date.strftime("%M") ) # minute 0 -59
@@ -40,38 +40,38 @@ def process(svg_document,data,params,internal_state) :
     ydiv = params.get("Ydiv")
 
     for point in data.get_current():
-      aggregate += float(point['value'])
-      #allow user to determine how fast the graph is drawn again
-      number = get_minute(point['time'])
-      #work out where to draw
-      cell_width = params.get("Width") / xdiv
-      cell_height = params.get("Height") / ydiv
-      startx = (number % xdiv) * cell_width
-      starty = math.ceil(number / ydiv) * cell_height
-      startx += cell_width / 2
-      starty += cell_height / 2
-      #if we move to a new cell, start small again
-      if startx != internal_state.get("last_x",0) or starty != internal_state.get("last_y",0):
-          internal_state["last_x"] = startx
-          internal_state["last_y"] = starty
-          print "reset square num"
-          square_num = 0
+        aggregate += float(point['value'])
+        #allow user to determine how fast the graph is drawn again
+        number = get_minute(point['time'])
+        #work out where to draw
+        cell_width = params.get("Width") / xdiv
+        cell_height = params.get("Height") / ydiv
+        startx = (number % xdiv) * cell_width
+        starty = math.ceil(number / ydiv) * cell_height
+        startx += cell_width / 2
+        starty += cell_height / 2
+        #if we move to a new cell, start small again
+        if startx != internal_state.get("last_x",0) or starty != internal_state.get("last_y",0):
+            internal_state["last_x"] = startx
+            internal_state["last_y"] = starty
+            print "reset square num"
+            square_num = 0
         
-      print "number:%d\naggregate:%.2f" % (number, aggregate)
-      print "x:%d y:%d" % ( startx, starty )
+        print "number:%d\naggregate:%.2f" % (number, aggregate)
+        print "x:%d y:%d" % ( startx, starty )
 
-      #if we have aggregated enough values to draw a square
-      while aggregate > params.get("Value"):
-          width = params.get("SquareInc") + square_num * params.get("SquareInc")
-          print "square #%d width %d" % ( square_num, width )
-          if params.get("Rotate"):
-            rotate = int(square_num*params.get("Rotate")) % 360 
-          else:
-            rotate = 0
-          square( startx,starty,width,svg_document,number,rotate)
-          aggregate -= params.get("Value")
-          #increment squares
-          square_num += 1
+        #if we have aggregated enough values to draw a square
+        while aggregate > params.get("Value"):
+            width = params.get("SquareInc") + square_num * params.get("SquareInc")
+            print "square #%d width %d" % ( square_num, width )
+            if params.get("Rotate"):
+                rotate = int(square_num*params.get("Rotate")) % 360 
+            else:
+                rotate = 0
+            square( startx,starty,width,svg_document,number,rotate)
+            aggregate -= params.get("Value")
+            #increment squares
+            square_num += 1
 
     internal_state["aggregate"]=aggregate
     internal_state["square_num"]=square_num
@@ -95,9 +95,9 @@ def can_run(data,params,internal_state):
     #run every time
     aggregate = internal_state.get("aggregate",0)
     for point in data.get_current():
-      aggregate += float(point['value'])
-      if aggregate > params.get("Value"):
-        return True
+        aggregate += float(point['value'])
+        if aggregate > params.get("Value"):
+            return True
     print "aggregate %f < value %f so not running" % ( aggregate, params.get("Value") )
     return False
 
