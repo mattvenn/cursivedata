8f13b276533ef18d9ab7c50fda29f00e649c9042
mo-seph <dave@mo-seph.com>
2013-02-02 18:07:56 +0000

    Changeing test_shape to work with new drawing code

--- testsite/generators/test_shape.py
+++ testsite/generators/test_shape.py
@@ -2,24 +2,21 @@ import pysvg.text
 import colorsys
 from pysvg.builders import *
 
+#Not used now. Left for posterity and how to do paths
 def square(x,y,width,height,dwg):
-  points = []
-  style_dict = { "fill":"none", "stroke":"#000", "stroke-width":"1" }
+    style_dict = { "fill":"none", "stroke":"#000", "stroke-width":"1" }
+    p = path("M%d,%d" % (x,y))
+    p.appendLineToPath(x+width,y,False)
+    p.appendLineToPath(x+width,y+height,False)
+    p.appendLineToPath(x,y+width,False)
+    p.appendLineToPath(x,y,False)
+    p.set_style(StyleBuilder(style_dict).getStyle())
+    p.set_id(id)
+    dwg.addElement(p)
 
-  p = path("M%d,%d" % (x,y))
-  p.appendLineToPath(x+width,y,False)
-  p.appendLineToPath(x+width,y+height,False)
-  p.appendLineToPath(x,y+width,False)
-  p.appendLineToPath(x,y,False)
-  p.set_style(StyleBuilder(style_dict).getStyle())
-  p.set_id(id)
-  dwg.addElement(p)
-
-def process(svg_document,data,params,internal_state) :
-    print "Example Processing data with parameters",map(str,params)
-    #print "Example internal state: ",internal_state
-    #print "Internal State: ",internal_state.get("i","None")
-    square(params.get('x'),params.get('y'),params.get('Width'),params.get('Height'),svg_document) 
+def process(drawing,data,params,internal_state) :
+    print "Drawing an example rectangle",map(str,params)
+    drawing.rect(params.get('x'),params.get('y'),params.get('Width'),params.get('Height')) 
     return None
 
 def get_params() :
