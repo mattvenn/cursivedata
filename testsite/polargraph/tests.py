1d1bfb85659826b5d1ba7e3d8d0324f106877de1
Matt Venn <matt@mattvenn.net>
2013-02-14 10:29:41 +0000

    made a better square test and added a new one

--- testsite/polargraph/tests.py
+++ testsite/polargraph/tests.py
@@ -1,8 +1,5 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+run "manage.py test".
 """
 
 import pdb
@@ -23,13 +20,6 @@ from pysvg.parser import parse
 from django.utils import timezone
 import nose.tools as nt
 
-"""
-#class SimpleTest(TestCase):
-class SimpleTest():
-    def test_basic_addition(self):
-        self.assertEqual(1 + 1, 2)
-
-"""
 class TestPipeline():
 
     def setup(self):
@@ -43,10 +33,11 @@ class TestPipeline():
         self.data_store = DataStore()
         self.data_store.save()
 
-        self.end_point = Endpoint(name="My Robot",device="Polargraph",location="Under the stairs" , width=500, height=400, top_margin=100, side_margin=100)
+        self.end_point = Endpoint(name="My Robot",device="Polargraph",location="Under the stairs" , width=1000, height=1000, top_margin=100, side_margin=100)
         self.end_point.save()
 
         self.pipeline = Pipeline(name="Test Pipeline" ,data_store=self.data_store, generator=self.generator, endpoint=self.end_point,state=self.gen_state, last_updated=timezone.now() )
+        #set to same size as robot
 
         self.svg_doc =self.pipeline.create_svg_doc()
         self.drawing = Drawing(self.svg_doc)
@@ -97,13 +88,80 @@ class TestPipeline():
         nt.assert_equal(self.data_store.load_current()[0]['value'],data[0]['value'] )
         #would be good to check time deserialization
     """
+   
+    def test_100mm_square(self):
+        sq_height = 100
+        sq_width  = 100
 
+        self.gen_state.params['x']=0
+        self.gen_state.params['y']=0
+        self.gen_state.params['Width']=sq_width
+        self.gen_state.params['Height']=sq_height
+        self.gen_state.save()
+
+        self.run_and_check_size(sq_height,sq_width)
+
+    def test_200mm_square(self):
+        sq_height = 200
+        sq_width  = 200
+
+        self.gen_state.params['x']=0
+        self.gen_state.params['y']=0
+        self.gen_state.params['Width']=sq_width
+        self.gen_state.params['Height']=sq_height
+        self.gen_state.save()
+
+        self.run_and_check_size(sq_height,sq_width)
+
+    def run_and_check_size(self,sq_height,sq_width):
+        #this runs the pipeline
+        data = [{ 'value' : 4000 }]
+        self.data_store.add_data(data)
+
+        #robot margins
+        side_m = self.end_point.side_margin
+        top_m = self.end_point.top_margin
+        
+        gcode = open(self.end_point.get_next_filename()).readlines()
+        gcode = [ s.strip() for s in gcode ]
+
+        #better way to do this?
+        nt.assert_equal(gcode[0],'d0')
+        nt.assert_equal(gcode[1],'g%d.0,%d.0' % (side_m,top_m+sq_height))
+        nt.assert_equal(gcode[2],'d1')
+        nt.assert_equal(gcode[3],'g%d.0,%d.0' % (side_m+sq_width,top_m+sq_height))
+        nt.assert_equal(gcode[4],'g%d.0,%d.0' % (side_m+sq_width,top_m))
+        nt.assert_equal(gcode[5],'g%d.0,%d.0' % (side_m,top_m))
+        nt.assert_equal(gcode[6],'g%d.0,%d.0' % (side_m,top_m+sq_height))
+        nt.assert_equal(gcode[7],'d0')
+
+    """
+    self.pipeline.update_size(300,300)
+    self.pipeline.print_width = 300
+    self.pipeline.print_top_left_x = 0
+    self.pipeline.print_top_left_y = 0
+    self.pipeline.save()
+    """
+    """
     def test_run_pipeline(self):
         #data is what is needed to run pipeline
         data = [{ 'value' : 4000 }]
+
+        #set robot margins
+        end_point = Endpoint(name="My Robot",device="Polargraph",location="Under the stairs" , width=500, height=400, top_margin=100, side_margin=100)
+        #set pipeline so there is no scaling
         self.pipeline.update_size(300,300)
         self.pipeline.print_width = 300
+        self.pipeline.print_top_left_x = 0
+        self.pipeline.print_top_left_y = 0
         self.pipeline.save()
+        self.gen_state.params['x']=0
+        self.gen_state.params['y']=0
+        self.gen_state.params['Width']=100
+        self.gen_state.params['Height']=100
+        self.gen_state.save()
+
+        #this runs the pipeline
         self.data_store.add_data(data)
         gcode = open(self.end_point.get_next_filename()).readlines()
         gcode = [ s.strip() for s in gcode ]
@@ -117,6 +175,8 @@ class TestPipeline():
         nt.assert_equal(gcode[5],'g100.0,100.0')
         nt.assert_equal(gcode[6],'g100.0,200.0')
         nt.assert_equal(gcode[7],'d0')
+        
+    """
 
 
 
