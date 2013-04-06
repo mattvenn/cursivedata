294dfbd0e6daf3461ae3829306876cf2a14a19a0
Matt Venn <matt@mattvenn.net>
2013-04-06 21:03:47 +0100

    replaced link with file

--- client/config.h
+++ client/config.h
@@ -1,1 +1,21 @@
-/home/matthew/work/arduinosketchbook/energyPlotter/leonardoTests/config.h
+struct config_t
+{
+  int stepsPerRevolution;
+  float stepsPerMM;
+  int hanger_l; 
+  int top_margin; 
+  int side_margin; 
+  float width; 
+  float height; 
+  int left_limit; //142mm
+  int right_limit; //152mm
+  byte id;
+  float gondola_width; 
+  int default_pwm;
+  int lowpower_pwm;
+  int home_pwm_high;
+  int home_pwm_low;
+  int home_speed;
+  int maxSpeed;
+} 
+config;
