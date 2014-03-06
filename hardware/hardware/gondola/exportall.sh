393252f05647b7bf6e379db162eb8645b482ee2f
Matt Venn <matt@mattvenn.net>
2014-03-06 11:43:49 +0000

    export cam

--- hardware/hardware/gondola/exportall.sh
+++ hardware/hardware/gondola/exportall.sh
@@ -3,3 +3,4 @@ openscad gondola.scad -D 'export_leaf_riser=true' -x leaf_riser.dxf
 openscad gondola.scad -D 'export_servo_mount=true' -x servo_mount.dxf
 openscad gondola.scad -D 'export_servo_support=true' -x servo_support.dxf
 openscad gondola.scad -D 'export_gondola=true' -x gondola.dxf
+openscad gondola.scad -D 'export_cam=true' -x cam.dxf
