a521cd89973f8499c9222a6112cecff9974516fe
matt venn <matt@mattvenn.net>
2015-01-24 14:17:52 +0000

    merged old svg functions in

--- cursivelib/svg.py
+++ cursivelib/svg.py
@@ -113,3 +113,33 @@ def create_svg_doc(width,height,createRect=False):
         doc.addElement(build.createRect(0, 0, width="100%", height="100%", fill = "rgb(255, 255, 255)"))
     return doc
 
+'''
+Created on 6 Jan 2013
+
+@author: dmrust
+'''
+import time,copy
+from pysvg.parser import parse
+import time
+import sys
+import cairosvg
+from xml.parsers.expat import ExpatError
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ParseError
+
+
+def convert_svg_to_png( svgfile, pngfilename ):
+    with open( pngfilename, 'w+') as png_file:
+        #print "Writing PNG file:",pngfilename," from ",svgfile," got",str(png_file)
+        cairosvg.svg2png(url=svgfile,write_to=png_file)
+
+def get_temp_filename(extension):
+    millis = int(round(time.time() * 1000000))
+    return "tmp/tmpfile"+str(millis)+"."+extension
+
+def write_temp_svg_file( svgstring ):
+    fn = get_temp_filename("svg")
+    with open( fn, "wb" ) as outfile:
+        outfile.write( svgstring)
+    
+    return fn
