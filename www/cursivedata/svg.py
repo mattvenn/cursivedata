fdf4f0d70b5d6236d95d54bc3b524f9363eb1539
matt venn <matt@mattvenn.net>
2015-01-24 14:19:26 +0000

    exit on all functions to see if it ever gets used

--- www/cursivedata/svg.py
+++ www/cursivedata/svg.py
@@ -14,15 +14,18 @@ from xml.etree.ElementTree import ParseError
 
 
 def convert_svg_to_png( svgfile, pngfilename ):
+    exit(1)
     with open( pngfilename, 'w+') as png_file:
         #print "Writing PNG file:",pngfilename," from ",svgfile," got",str(png_file)
         cairosvg.svg2png(url=svgfile,write_to=png_file)
 
 def get_temp_filename(extension):
+    exit(1)
     millis = int(round(time.time() * 1000000))
     return "tmp/tmpfile"+str(millis)+"."+extension
 
 def write_temp_svg_file( svgstring ):
+    exit(1)
     fn = get_temp_filename("svg")
     with open( fn, "wb" ) as outfile:
         outfile.write( svgstring)
