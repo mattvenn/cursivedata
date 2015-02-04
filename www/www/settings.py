1f6957ab7897e5fbcc0d38bd4b103171d51a13df
matt venn <matt@mattvenn.net>
2015-02-04 20:05:55 +0000

    gunicorn + nginx changes

--- www/www/settings.py
+++ www/www/settings.py
@@ -41,7 +41,7 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Greenwich'
+TIME_ZONE = 'GMT'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
