ee722e53c21d53b551d520743931f08056e65872
matt venn <matt@mattvenn.net>
2015-04-08 09:27:31 +0100

    eds updated animation

---
+++ www/media/animation/animation.js
@@ -0,0 +1,111 @@
+'use strict'
+
+var SVGAnimation = {
+
+	/* Set behaviour via these properties */
+	
+	refreshSpeed : 500, // Original method = 25% CPU usage, current = 35%
+	_playing : false,
+	
+	/* Handler methods */
+	
+	setup : function() {
+		var frameEl = document.getElementById( 'frame' ),
+			speed = document.getElementById( 'speed' ),
+			SVGDocument = document.getElementById( 'embed' ).getSVGDocument().children[0],
+			elements = SVGDocument.getElementsByClassName( 'frame' );
+		frameEl.min = parseInt( elements[0].id );
+		frameEl.max = parseInt( elements[elements.length-1].id );
+		frameEl.value = parseInt( frameEl.max );
+		//frameEl.dataset.oldValue = '' + frameEl.max;
+		speed.min = parseInt( this.refreshSpeed );
+		speed.max = parseInt( this.refreshSpeed * 10 );
+		speed.value = parseInt( this.refreshSpeed );
+		SVGDocument.children[0].setAttribute( 'id', 'background' );
+		this._reverseNodes( SVGDocument );
+		this.setFrame( frameEl.max );
+	},
+	play : function() {
+		var me = this,
+			SVGDocument = document.getElementById( 'embed' ).getSVGDocument().children[0],
+			background = SVGDocument.getElementById( 'background' ),
+			frameEl = document.getElementById( 'frame' ),
+			frameCount = parseInt( frameEl.value ),
+			speed = parseInt( document.getElementById( 'speed' ).value );
+		this.stop();
+		this._playing = true;
+		this.animationCounter = setInterval( function frameTimer() {
+			frameCount = frameCount + speed;
+			if ( frameCount >= frameEl.max ) {
+				me.stop();
+				return;
+			}
+			frameEl.value = frameCount;
+			me._moveNode( background, me._getNearestFrame( frameCount, frameEl.max ) );
+			me._updateTime();
+		}, this.refreshSpeed );
+	},
+	playFromStart : function() {
+		var frameEl = document.getElementById( 'frame' );
+		frameEl.value = frameEl.min;
+		this.play();
+	},
+	stop : function() {
+		clearInterval( this.animationCounter );
+		this._playing = false;
+	},
+	setFrame : function( $frame ) {
+		var SVGDocument = document.getElementById( 'embed' ).getSVGDocument().children[0],
+			background = SVGDocument.getElementById( 'background' ),
+			frameEl = document.getElementById( 'frame' );
+		if ( !!$frame ) {
+			frameEl.value = $frame;
+		}
+		this.stop();
+		this._moveNode( background, this._getNearestFrame( frameEl.value, frameEl.max ) );
+		this._updateTime();
+	},
+	changeSpeed : function() {
+		if ( this._playing === true ) {
+			this.play();
+		}
+	},
+	
+	/* Functions of specific use for SVGAnimation */
+	
+	_getNearestFrame : function( $startFrame, $stopFrame ) {
+		var i = $startFrame, elem = null,
+			SVGDocument = document.getElementById( 'embed' ).getSVGDocument();
+		for ( ; elem === null && i <= $stopFrame; i++ ) {
+			elem = SVGDocument.getElementById( '' + i );
+		}
+		return elem;
+	},
+	_updateTime : function() {
+		var frameText = document.getElementById( 'rangeVal' );
+		frameText.value = this._getTime( frame.value );
+	},	
+		
+	/* Generically useful functions */
+	
+	_getTime : function( $secs ) { // Currently inactive
+		var t = new Date( 1970, 0, 1 );
+		t.setSeconds( $secs );
+		return ( '0' + t.getHours() ).slice( -2 ) + ':' + ( '0' + t.getMinutes() ).slice( -2 ) + ':' + ( '0' + t.getSeconds() ).slice( -2 );
+	},
+	_moveNode : function( $node, $relation ) {
+		$node.parentNode.insertBefore( $node, $relation );
+	},
+	_reverseNodes : function( $node ) {
+		var parentNode = $node.parentNode, nextSibling = $node.nextSibling,
+			frag = $node.ownerDocument.createDocumentFragment();
+		parentNode.removeChild( $node );
+		while( $node.lastChild ) {
+			frag.appendChild( $node.lastChild );
+		}
+		$node.appendChild( frag );
+		parentNode.insertBefore( $node, nextSibling );
+		return $node;
+	}
+}
+
