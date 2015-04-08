13f2ffa7ec2b5a4d9f010eac1404fe2947b4e2c2
matt venn <matt@mattvenn.net>
2015-04-08 09:23:22 +0100

    better return for when missing gcodes

--- www/cursivedata/views.py
+++ www/cursivedata/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.forms.extras.widgets import SelectDateWidget
-from django.http import HttpResponse, Http404, Http204, HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.servers.basehttp import FileWrapper
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
@@ -394,7 +394,7 @@ def get_gcode(request, endpointID ):
         raise Http404
     if not filename:
         log.info("no gcodes waiting to be served")
-        raise Http204
+        return HttpResponse(status=204)
 
     log.debug("filename = %s" % filename)
     consume = request.REQUEST.get('consume', False)
