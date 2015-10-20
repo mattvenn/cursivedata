34fcb77b4c15a357798641b80dfa426c128cc7ac
matt venn <matt@mattvenn.net>
2015-10-20 22:14:05 +0100

    servo dev and tests

--- hardware/model/move_model/conf.py
+++ hardware/model/move_model/conf.py
@@ -2,9 +2,10 @@ conf = {
     'plan_len' : 10,    # cm
     'max_spd' : 10.0,
     'min_spd' : 0.1,
-    'spd_err' : 0.0,  # % error in speed measurement of servo
     'acc' : 0.1,
-    'len_err' : 0.5,   # random length err up to this in cm
+    'spd_err' : 5.0,  # allowable servo speed error
+    'len_err' : 10,   # allowable servo position error: must be >= max_spd
+    'servo_acc' : 1.0,  # amount speed increased by servo per cycle
     'width' : 700,
     'height' : 500,
     'scaling' : 8, # how much bigger to make the png than the robot
--- hardware/model/move_model/servo.py
+++ hardware/model/move_model/servo.py
@@ -3,11 +3,10 @@ import random
 from conf import conf
 log = logging.getLogger(__name__)
 
-
 class Servo():
     def __init__(self, len=100, name=''):
         self.len = len
-        self.target_len = len
+        self.targ_len = len
         self.spd_err = conf['spd_err']
         self.name = name
         self.updates = 0
@@ -16,18 +15,22 @@ class Servo():
         self.targ_spd = 0
         self.finish = True
     
-    def set_len(self, targ_len, targ_spd=0):
-        self.updates = 0
+    def set_len(self, targ_len, targ_spd):
         self.finish = False
+        self.start_spd = self.spd
         self.targ_spd = targ_spd
-        # limit speeds - needs to work for -ve too
-        """
-        if targ_spd > conf['max_spd']:
-            log.error("%s: limiting speed from %.2f to %.2f!" % (self.name, self.targ_spd, conf['max_spd']))
-            self.targ_spd = conf['max_spd']
-        """
+        # limit speeds
+        #if targ_spd > conf['max_spd']:
+        #    log.error("%s: limiting speed from %.2f to %.2f!" % (self.name, speed, self.max_spd_lmt))
+        #    self.targ_spd = conf['max_spd']
+
 
         self.targ_len = targ_len
+
+        # how long it should roughly take
+        t = self.len_error() / self.spd_error()
+        self.spd_change_time = self.calculate_acc_time(t)
+        log.debug(self.spd_change_time)
 
     def finished(self):
         return self.finish
@@ -35,6 +38,49 @@ class Servo():
     def get_len(self):
         return self.len
 
-    def log_error(self):
-        log.info("%s: len err=%.2f, spd err=%.2f" % (self.name, self.len - self.targ_len, self.spd - self.targ_spd))
+    def update(self):
+        self.updates += 1
 
+        log.debug("{self.name}: len={self.len:.2f}, target={self.targ_len:.2f} spd={self.spd:.3f} targ_spd={self.targ_spd:.3f}".format(**locals()))
+        #self.len += self.step + speed_error
+
+        if self.updates > self.spd_change_time:
+            self.spd += self.targ_spd - self.conf['servo_acc']
+
+        if self.targ_len < self.len:
+            self.spd += conf['servo_acc']
+
+        if self.spd > conf['max_spd']:
+            self.spd = conf['max_spd']
+        if self.spd < - conf['max_spd']:
+            self.spd = - conf['max_spd']
+
+        self.len += self.spd
+
+        if self.len_error() < conf['len_err']:
+            self.finish = True
+       
+    def len_error(self):
+        return abs(self.len - self.targ_len)
+
+    def spd_error(self):
+        return abs(self.spd - self.targ_spd)
+
+    def calculate_acc_time(self, t):
+        
+        s_s = float(self.spd)
+        l = self.len_error()
+        s_t = float(self.targ_spd)
+
+        # check values first
+        if (abs(s_s - s_t) / t) > conf['servo_acc']:
+            raise Exception("impossible to acc/dec in time")
+
+        td = ( 2 * l - t * abs(s_t - s_s)) / abs(s_t - s_s) 
+        if s_t > s_s:
+            td = t - td 
+        if td < 0:
+            raise Exception("impossible to acheive length in time")
+        return td
+
+        
