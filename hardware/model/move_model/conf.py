cb4a2976403da582b625c433c8c51892ff46bb9e
matt venn <matt@mattvenn.net>
2015-11-27 19:11:19 +0000

    config for 600mm bot

--- hardware/model/move_model/conf.py
+++ hardware/model/move_model/conf.py
@@ -1,8 +1,8 @@
 conf = {
-    'plan_len' : 50,    # cm
-    'max_spd' : 5.0,
-    'acc' : 0.1,
-    'width' : 700,
+    'plan_len' : 5.1,    # cm
+    'max_spd' : 10.0,
+    'acc' : 0.02,
+    'width' : 600,
     'height' : 500,
     'scaling' : 8, # how much bigger to make the png than the robot
     'corner_spd_tune' : 0.01,
