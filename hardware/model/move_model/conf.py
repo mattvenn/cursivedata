34e501d36e1c65146d9ed0ed32dceb611d154bb0
matt venn <matt@mattvenn.net>
2015-12-16 14:46:47 +0000

    conf

--- hardware/model/move_model/conf.py
+++ hardware/model/move_model/conf.py
@@ -1,8 +1,8 @@
 conf = {
     'plan_len' : 20,    # cm
-    'max_spd' : 20.0,
+    'max_spd' : 15.0,
     'acc' : 0.15,
-    'width' : 1830,
+    'width' : 1705 - 130,
     'height' : 2000,
     'scaling' : 8, # how much bigger to make the png than the robot
     'corner_spd_tune' : 0.01,
