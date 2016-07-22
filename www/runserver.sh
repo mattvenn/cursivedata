830ca7f7f3add4ded34ea67bfa563f40d93f4b64
matt venn <matt@mattvenn.net>
2016-07-22 14:46:19 +0200

    moved to new server

--- www/runserver.sh
+++ www/runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /home/polarsite/.virtualenvs/polarsite/bin/activate
 
 cd /home/polarsite/cursivedata/www/
-exec gunicorn www.wsgi:application -b localhost:8000 -w 4
+source venv/bin/activate
+exec gunicorn www.wsgi:application -b localhost:8000 -w 2
